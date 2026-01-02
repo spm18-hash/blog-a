f81ca3b80013aee7755e53e6175807bea54bd018
Smit Mistry <smitmistry19@gmail.com>
2026-01-02 14:14:27 +0530

    updated pagination, table of content component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blog-a-brown.vercel.app",
   output: "static",
+
   vite: {
     plugins: [tailwindcss({ optimize: true })],
   },
+
   prefetch: { prefetchAll: true },
+
   experimental: {
     svgo: true,
   },
+
+  integrations: [sitemap()],
 });
